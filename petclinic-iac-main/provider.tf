3b8c231c6840deb10b72237ab1e34696dc6ce4fc
arthurbensaid <arthur.bensaid@gmail.com>
2024-04-08 17:45:23 +0200

    raccording_rds

--- petclinic-iac-main/provider.tf
+++ petclinic-iac-main/provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region = "eu-west-3"
+  profile = "student35_jan24_bootcamp_devops_petclinic"
 }
 
 terraform {
@@ -12,6 +13,7 @@ terraform {
 
   backend "s3" {
     bucket = "petclinic-devops"
+    profile = "student35_jan24_bootcamp_devops_petclinic"
     key    = "tfstatefiles/terraform.tfstate"
     region = "eu-west-3"
   }
